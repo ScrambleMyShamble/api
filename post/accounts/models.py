7593c0eb52cd50c827ad9cae64abbcef4c70afd0
major <majorofffpavel@gmail.com>
2022-03-27 10:51:25 +0300

    добавил приложение accounts

---
+++ post/accounts/models.py
@@ -0,0 +1,84 @@
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+# Create your models here.
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, email, password=None, name=None, full_name=None,
+                    is_active=True, staff=None, admin=None):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not email:
+            raise ValueError("Пользователь должен иметь email адрес")
+        if not password:
+            raise ValueError('Пользователь должен ввести пароль')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password)
+        user.staff = staff
+        user.admin = admin
+        user.is_active = is_active
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, name=None):
+        user = self.create_user(email, password=password, name=name,
+                                staff=True, admin=True)
+        return user
+
+    def create_staffuser(self, email, password=None, name=None):
+        user = self.create_user(email, password=password, name=name,
+                                staff=True, admin=False)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True, max_length=255)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    full_name = models.CharField(max_length=255, blank=True, null=True)
+    stuff = models.BooleanField(default=False)
+    admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_short_name(self):
+        if self.name:
+            return self.name
+        return self.email
+
+    def get_full_name(self):
+        if self.full_name:
+            return self.full_name
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        if self.admin:
+            return True
+        return self.stuff
+
+    @property
+    def is_admin(self):
+        if self.admin:
+            return self.admin
+
+    def save(self, *args, **kwargs):
+        print(self.password)
+        super().save(*args, **kwargs)
